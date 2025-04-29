d443a36299ba3517b48ecff52853e7894a61484b
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-04-29 14:34:33 -0300

    fix: verificar url por youtube.com

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,7 @@ function App() {
     setError('');
 
     // Se detectar um link direto do YouTube
-    if (
-      query.includes('https://www.youtube.com/watch') || query.includes('https://www.youtube.com/watch') ||
-      query.includes('http://www.youtube.com/watch') || query.includes('http://www.youtube.com/watch')
-    ) {
+    if (query.includes('youtube.com/watch')) {
       const url = new URL(query);
       const videoId = url.searchParams.get('v');
       if (videoId) {
