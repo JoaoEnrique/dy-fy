e2a8fb9890f1dd27db73b847df0d93740a8213da
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-04-29 17:40:53 -0300

    test: deploy

--- src/utils/vars.ts
+++ src/utils/vars.ts
@@ -5,3 +5,4 @@ export const BACKEND_DOWNLOAD_URL = process.env.REACT_APP_BACKEND_DOWNLOAD_URL |
 export const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY || "http://localhost:5000";
 export const YOUTUBE_SEARCH_URL = process.env.REACT_APP_YOUTUBE_SEARCH_URL || "https://www.googleapis.com/youtube/v3/search";
 export const BACK_OR_YOUTUBE: BackOrYoutube = (process.env.REACT_APP_BACK_OR_YOUTUBE as BackOrYoutube) || "back";
+ 
